d487ab00d8cd14eebcbe26f96487031513e193cd
Alexey <lehandro567@mail.ru>
2021-10-18 19:56:24 +0400

    revision auto-write-text

--- Auto Write Text/Auto-write.js
+++ Auto Write Text/Auto-write.js
@@ -1,11 +1,9 @@
 let text = "Спасибо, что зашли на этот репозиторий и посмотрели эти небольшие забавные штуки <3";
-
 let index = 0;
 
 function writeText() {
-    document.body.innerText = text.slice(0,index);
+    document.getElementById("demo").innerHTML= text.slice(0,index);
     index++;
-
     if(index > text.length) {
         index = 0;
     }
